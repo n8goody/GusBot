251a6a319d1fe863ada519ff8d77cd5e38266bcc
Nate <n8goody@gmail.com>
2020-09-05 18:11:11 -0500

    Added toggleTTS and wordList commands.

--- bot.py
+++ bot.py
@@ -6,6 +6,8 @@ adjectivesFile = 'rcAdjectives.txt'
 nounsFile = 'rcNouns.txt'
 verbsFile = 'rcVerbs.txt'
 
+isTTSon = True
+
 bot = commands.Bot(command_prefix = 'g!')
 
 # creates dictionary of lists, each list a different letter, for genRoomName from csv file
@@ -24,6 +26,49 @@ def fileToDict(filename):
 @bot.event
 async def on_ready():
     print("Bot is ready")
+
+@bot.command()
+async def toggleTTS(ctx):
+    global isTTSon
+    if isTTSon:
+        isTTSon = False
+        await ctx.send('TTS is OFF')
+    else:
+        isTTSon = True
+        await ctx.send('TTS is ON')
+
+@bot.command()
+async def wordList(ctx, type, letter):
+    l = str(letter).strip().upper()
+    t = str(type).strip().upper()
+    if t =='N':
+        typeFile = 'rcNouns.txt'
+    elif t == 'A':
+        typeFile ='rcAdjectives.txt'
+    elif t == 'V':
+        typeFile = 'rcVerbs.txt'
+    else:
+        await ctx.send("Invalid Type. Valid types: N = nouns; A = Adjectives; V = Verbs")
+        return
+
+    if (not l.isalpha() and  not l =='%'):
+        await ctx.send("Invalid letter. Must be a letter or '%'. '%' = all letters. No special characters/numbers")
+        return
+    if l == '%':
+        typeDict = fileToDict(typeFile)
+        output = ''
+        for key in typeDict:
+            output += key + ': '
+            for word in typeDict[key]:
+                output += word + ' '
+            output += '\n'
+        await ctx.send(output) 
+    else:
+        typeDict =fileToDict(typeFile)
+        output = l + ': '
+        for word in typeDict[l]:
+            output += word + ' '
+        await ctx.send(output) 
 
 
 
@@ -60,7 +105,7 @@ async def genRoomName(ctx, arg):
         randNum = random.randint(0,listLen-1)
         roomPhrase += nounDict[char][randNum] + ' '
 
-        await ctx.send('The room code is '+ roomPhrase, tts=True)
+        await ctx.send('The room code is '+ roomPhrase, tts=isTTSon)
         
     else:
         await ctx.send("Room Code must be exactly 4 letters (no numbers/special characters)")
